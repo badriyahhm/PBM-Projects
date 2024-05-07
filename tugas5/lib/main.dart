d73954f5101e46464e6f004856719b3fa0ddf4c0
Badriyah <badriyahhusnayah@gmail.com>
2024-05-07 17:25:58 +0700

    mengubah tampilan

--- tugas5/lib/main.dart
+++ tugas5/lib/main.dart
@@ -1,125 +1,186 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+// membuat object quizBrain
+QuizBrain quizBrain = QuizBrain();
+
+// main Method
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizzlerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuizzlerApp extends StatelessWidget {
+  const QuizzlerApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: Color(0XFFF7F2EC),
+        appBar: AppBar(
+          title: Text(
+            'Genshin Quiz',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Color(0XFFffe09a),
+          centerTitle: true,
+        ),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuizPage> createState() => _QuizPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QuizPageState extends State<QuizPage> {
+  // array/list untuk menyimpan score, berupa Icon check or close.
+  List<Icon> scoreKeeper = [];
 
-  void _incrementCounter() {
+  // menghitung jawaban benar dan salah
+  int numCorrect = 0;
+  int numIncorrect = 0;
+
+  // fungsi mengecek jawaban dari user (parameter userPickedAnswer)
+  // ke jawaban dari daftar pertanyaan (list questions/_questionBanks)
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    // setState
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      //TODO: Step 4 - menggunakan IF/ELSE untuk cek akhir quiz
+      //jika ia, maka ke line berikuntya
+      if (quizBrain.isFinished() == true) {
+        //TODO Step 4 Part A - menampilkan alert menggunakan rFlutter_alert
+        Alert(
+          context: context,
+          title: 'Finished',
+          desc: 'You\'ve reached the end of the Quiz\n'
+              'Correct Answers: $numCorrect\n'
+              'Incorrect Answers: $numIncorrect\n'
+              'Total Questions: ${quizBrain.getTotalQuestions()}',
+        ).show();
+
+        quizBrain.reset();
+        numCorrect = 0;
+        numIncorrect = 0;
+        scoreKeeper = [];
+      }
+
+      //TODO: Step 6 - if not reached the end, ELSE do the answer checking
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(const Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+          numCorrect++;
+        } else {
+          scoreKeeper.add(const Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+          numIncorrect++;
+        }
+        quizBrain.nextQuestion();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 10.0), // Adjust horizontal padding
+            child: Center(
+              child: Text(quizBrain.getQuestionText(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 25.0,
+                    color: Color(0xFF272727),
+                  )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 180,
+                    child: TextButton(
+                      onPressed: () {
+                        checkAnswer(true);
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0xff8db3ef)),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        textStyle: MaterialStateProperty.all(
+                          TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      child: const Text(
+                        "TRUE",
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Container(
+                    height: 180,
+                    child: TextButton(
+                      onPressed: () {
+                        checkAnswer(false);
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0XFFf47256)),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        textStyle: MaterialStateProperty.all(
+                          TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      child: const Text(
+                        "FALSE",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 80, 0, 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: scoreKeeper,
+          ),
+        ),
+      ],
     );
   }
 }
